5a006a348bff14834fd6b852452d105b91511516
abilicz <adrian.bilicz@spartez-software.com>
2021-12-08 16:56:29 +0100

    secret manager

--- main.tf
+++ main.tf
@@ -15,7 +15,15 @@ resource "google_cloud_run_service" "default" {
           name = "MONGO_URI"
           value = "mongo-uri"
         }
-
+        env {
+          name = "SECRET_ENV_VAR"
+          value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.my_secret.secret_id
+              key = "latest"
+            }
+          }
+        }
       }
     }
     metadata {
@@ -49,6 +57,32 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   location = google_cloud_run_service.default.location
   project = google_cloud_run_service.default.project
   service = google_cloud_run_service.default.name
-
   policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_secret_manager_secret_iam_member" "secret-access" {
+  secret_id = google_secret_manager_secret.secret.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  depends_on = [google_secret_manager_secret.secret]
+}
+
+# Enabling Secret Manager API
+resource "google_project_service" "secretmanager" {
+  project = google_cloud_run_service.default.project
+  service  = "secretmanager.googleapis.com"
+}
+
+resource "google_secret_manager_secret" "my_secret" {
+  secret_id = "my_secret"
+  project = google_cloud_run_service.default.project
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "my_secret_v1" {
+  secret      = google_secret_manager_secret.my_secret.id
+  secret_data = "my super secret data"
 }
